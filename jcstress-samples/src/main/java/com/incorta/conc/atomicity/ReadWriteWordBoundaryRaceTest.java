40faa4e78bd67e7576e5abefd385c4a7f426c5f4
Mostafa Elganainy <mostafa.elganainy@incorta.com>
2023-11-01 20:50:07 +0200

    Fix broken ReadWriteWordBoundaryRaceTest

--- jcstress-samples/src/main/java/com/incorta/conc/atomicity/ReadWriteWordBoundaryRaceTest.java
+++ jcstress-samples/src/main/java/com/incorta/conc/atomicity/ReadWriteWordBoundaryRaceTest.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package com.incorta.conc.atomicity;
 
 import com.incorta.conc.atomicity.OffHeapUtils;
@@ -34,19 +58,7 @@ public class ReadWriteWordBoundaryRaceTest {
     private static final Random RANDOM = new Random();
     private static final int CACHE_LINE_SIZE = 128;
 
-    static final VarHandle VH;
-
-    static {
-        try {
-            VH = MethodHandles.lookup().findVarHandle(ReadWriteWordBoundaryRaceTest.class, "v", long.class);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            throw new IllegalStateException(e);
-        }
-    }
-    
     private static final boolean alignAddresses = false;
-    
-    // private final ByteBuffer buffer;
     
     private final long page;
     private final long offset;
